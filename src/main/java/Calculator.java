1d06ec477769db42ddb776f2e00f780113c91bd1
zakiryanova <zakiryanova@localhost>
2019-10-15 23:53:19 +0300

    fixes #2

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,10 +1,14 @@
 public class Calculator {
+    int costLimit = 1000;
+    int discountLimit = 100;
+    double discountRate = 0.05;
+
     int calculateCost(int boardingCost, int costPerKm, int distance) {
         int taxiCost = boardingCost + costPerKm * distance;
-        if (taxiCost > 1000) {
-            double discount = taxiCost * 0.05;
-            if (discount > 100) {
-                discount = 100;
+        if (taxiCost > costLimit) {
+            double discount = taxiCost * discountRate;
+            if (discount > discountLimit) {
+                discount = discountLimit;
             }
             taxiCost = taxiCost - (int) discount;
         }
